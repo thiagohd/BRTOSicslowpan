7dc37586d0c52ca950a25697b26094e272ea92fe
Thiago Henrique Deicke <deicke@live.com>
2016-04-15 01:33:27 -0300

    Arquivos desnecessários

--- brtos-contiki-official-ClientUDP/contiki/examples/udp-rpl/sky-websense.c
+++ brtos-contiki-official-ClientUDP/contiki/examples/udp-rpl/sky-websense.c
@@ -30,33 +30,21 @@
 
 /**
  * \file
- *         A simple example using HTTP to control and be controlled
+ *         Light and temperatur sensor web demo
  * \author
- *         Niclas Finne <nfi@sics.se>
+ *         Niclas Finne    <nfi@sics.se>
  *         Joakim Eriksson <joakime@sics.se>
+ *         Joel Hoglund    <joel@sics.se>
  */
 
 #include "contiki.h"
-#include "dev/button-sensor.h"
-#include "dev/leds.h"
-#include "wget.h"
 #include "httpd-simple.h"
+//#include "dev/sht11/sht11-sensor.h"
+//#include "dev/light-sensor.h"
+#include "dev/leds.h"
 #include <stdio.h>
 
-/* The address of the server to register the services for this node */
-#define SERVER       "[aaaa::1]"
-
-/* This command registers two services (/0 and /1) to turn the leds on or off */
-#define REGISTER_COMMAND "/r?p=0&d=Turn%20off%20leds&p=1&d=Turn%20on%20leds"
-
-/* The address of the other node to control */
-#define OTHER_NODE   "[aaaa::1]"
-
-/* The commands to send to the other node */
-#define SET_LEDS_ON  "/1"
-#define SET_LEDS_OFF "/0"
-
-PROCESS(websense_remote_process, "Websense Remote");
+PROCESS(web_sense_process, "Sense Web Demo");
 PROCESS(webserver_nogui_process, "Web server");
 PROCESS_THREAD(webserver_nogui_process, ev, data)
 {
@@ -71,33 +59,91 @@ PROCESS_THREAD(webserver_nogui_process, ev, data)
 
   PROCESS_END();
 }
-//AUTOSTART_PROCESSES(&websense_remote_process,&webserver_nogui_process);
-void start_web_sense_server_task(void){
-	process_start(&resolv_process, NULL);
-	process_start(&udp_client_process, NULL);
-}
+AUTOSTART_PROCESSES(&web_sense_process,&webserver_nogui_process);
+/*void start_web_sense_server_task(void){
+	process_start(&web_sense_process, NULL);
+	process_start(&webserver_nogui_process, NULL);
+}*/
+
+#define HISTORY 16
+static int temperature[HISTORY];
+static int light1[HISTORY];
+static int sensors_pos;
 
 /*---------------------------------------------------------------------------*/
-static const char *TOP = "<html><head><title>Contiki Websense Remote</title></head><body>\n";
+static int
+get_light(void)
+{
+  return 10 * 7 / 7;
+}
+/*---------------------------------------------------------------------------*/
+static int
+get_temp(void)
+{
+  return ((3960 / 10) - 396) / 10;
+}
+/*---------------------------------------------------------------------------*/
+static const char *TOP = "<html><head><title>Contiki Web Sense</title></head><body>\n";
 static const char *BOTTOM = "</body></html>\n";
 /*---------------------------------------------------------------------------*/
+/* Only one single request at time */
+static char buf[256];
+static int blen;
+#define ADD(...) do {                                                   \
+    blen += snprintf(&buf[blen], sizeof(buf) - blen, __VA_ARGS__);      \
+  } while(0)
+static void
+generate_chart(const char *title, const char *unit, int min, int max, int *values)
+{
+  int i;
+  blen = 0;
+  ADD("<h1>%s</h1>\n"
+      "<img src=\"http://chart.apis.google.com/chart?"
+      "cht=lc&chs=400x300&chxt=x,x,y,y&chxp=1,50|3,50&"
+      "chxr=2,%d,%d|0,0,30&chds=%d,%d&chxl=1:|Time|3:|%s&chd=t:",
+      title, min, max, min, max, unit);
+  for(i = 0; i < HISTORY; i++) {
+    ADD("%s%d", i > 0 ? "," : "", values[(sensors_pos + i) % HISTORY]);
+  }
+  ADD("\">");
+}
 static
-PT_THREAD(handle_command(struct httpd_state *s))
+PT_THREAD(send_values(struct httpd_state *s))
 {
   PSOCK_BEGIN(&s->sout);
 
   SEND_STRING(&s->sout, TOP);
 
-  if(s->filename[1] == '0') {
+  if(strncmp(s->filename, "/index", 6) == 0 ||
+     s->filename[1] == '\0') {
+    /* Default page: show latest sensor values as text (does not
+       require Internet connection to Google for charts). */
+    blen = 0;
+    ADD("<h1>Current readings</h1>\n"
+        "Light: %u<br>"
+        "Temperature: %u&deg; C",
+        get_light(), get_temp());
+    SEND_STRING(&s->sout, buf);
+
+  } else if(s->filename[1] == '0') {
     /* Turn off leds */
     leds_off(LEDS_ALL);
     SEND_STRING(&s->sout, "Turned off leds!");
+
   } else if(s->filename[1] == '1') {
     /* Turn on leds */
     leds_on(LEDS_ALL);
     SEND_STRING(&s->sout, "Turned on leds!");
+
   } else {
-    SEND_STRING(&s->sout, "Unknown command");
+    if(s->filename[1] != 't') {
+      generate_chart("Light", "Light", 0, 500, light1);
+      SEND_STRING(&s->sout, buf);
+    }
+    if(s->filename[1] != 'l') {
+      generate_chart("Temperature", "Celsius", 15, 50, temperature);
+      SEND_STRING(&s->sout, buf);
+    }
   }
 
   SEND_STRING(&s->sout, BOTTOM);
@@ -108,79 +154,27 @@ PT_THREAD(handle_command(struct httpd_state *s))
 httpd_simple_script_t
 httpd_simple_get_script(const char *name)
 {
-  return handle_command;
+  return send_values;
 }
 /*---------------------------------------------------------------------------*/
-static void
-wget_done(int status)
+PROCESS_THREAD(web_sense_process, ev, data)
 {
-  switch(status) {
-  case WGET_OK:
-    printf("Command sent.\n");
-    break;
-  case WGET_TIMEDOUT:
-    printf("Command timedout.\n");
-    break;
-  case WGET_ABORTED:
-    printf("Connection aborted.\n");
-    break;
-  case WGET_CONNECT_FAILED:
-    printf("Failed to connect.\n");
-    break;
-  }
-}
-static const struct wget_callbacks callbacks = {
-  NULL, wget_done
-};
-static void
-send_command(const char *server, const char *command)
-{
-  int ret;
-  printf("Sending to [%s]: %s\n", server, command);
-  ret = wget_get(server, 80, command, &callbacks);
-  if(ret != WGET_OK) {
-    if(ret == WGET_ALREADY_RUNNING) {
-      printf("Waiting for previous command to finish.\n");
-    } else {
-      printf("Failed to send command: %u\n", ret);
-    }
-  }
-}
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(websense_remote_process, ev, data)
-{
-  static int mode;
   static struct etimer timer;
-
   PROCESS_BEGIN();
 
-  mode = 0;
-  wget_init();
+  sensors_pos = 0;
 
-  SENSORS_ACTIVATE(button_sensor);
-
-  /* Give the node some time to join the network before registering
-     the available services. */
-  etimer_set(&timer, CLOCK_SECOND * 30);
+  etimer_set(&timer, CLOCK_SECOND * 2);
+  //SENSORS_ACTIVATE(light_sensor);
+  //SENSORS_ACTIVATE(sht11_sensor);
 
   while(1) {
-    PROCESS_WAIT_EVENT();
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+    etimer_reset(&timer);
 
-    if(ev == sensors_event && data == &button_sensor) {
-      /* The button has been pressed. Send command to the other node. */
-      if(mode) {
-        /* Turn on leds */
-        send_command(OTHER_NODE, SET_LEDS_OFF);
-      } else {
-        send_command(OTHER_NODE, SET_LEDS_ON);
-      }
-      /* Alternate between the two commands */
-      mode = !mode;
-
-    } else if(ev == PROCESS_EVENT_TIMER && etimer_expired(&timer)) {
-      printf("Registering services\n");
-      send_command(SERVER, REGISTER_COMMAND);
-    }
+    light1[sensors_pos] = get_light();;
+    temperature[sensors_pos] = get_temp();
+    sensors_pos = (sensors_pos + 1) % HISTORY;
   }
 
   PROCESS_END();
